7e87cbf5e6dcecfa7932a9f944ee93e1f29fd507
matas.savickis <matas.savickis@4finance.com>
2024-01-11 20:21:14 +0200

    Initial commit

---
+++ cool-bank/src/test/java/com/coolbank/coolbank/CoolBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolbank.coolbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
